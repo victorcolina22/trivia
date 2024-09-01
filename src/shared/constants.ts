afee40b9906823e097cc57b4b72c334c718790c9
Victor Colina <victorcolina@G500606MAC272.local>
2024-08-31 20:39:26 -0400

    feat: Primer juego de trivia

---
+++ src/shared/constants.ts
@@ -0,0 +1,153 @@
+const CINEMA = [
+  {
+    id: 1,
+    question:
+      "¿Cuál es la película que generó más dinero en la década de los 2000?",
+    answer: "Paris",
+    position: 1,
+  },
+  {
+    id: 2,
+    question: "¿En qué locación ocurre la historia de Breaking Bad?",
+    answer: "Berlin",
+    position: 2,
+  },
+  {
+    id: 3,
+    question: "En la película 'Qué pasó ayer': ¿Cómo se llamaba el bebe?",
+    answer: "Rome",
+    position: 3,
+  },
+  {
+    id: 4,
+    question:
+      "¿Cuál es la fecha icónica en la que Cady habla con Aaron Samuels en Mean Girls/Chicas pesadas?",
+    answer: "Madrid",
+    position: 4,
+  },
+];
+
+const NEWS = [
+  {
+    id: 5,
+    question:
+      "¿Cómo se llama la mina de la que rescataron a los mineros en el 2010?",
+    answer: "Paris",
+    position: 1,
+  },
+  {
+    id: 6,
+    question:
+      "¿Qué país legalizó el matrimonio de personas del mismo sexo por primera vez? (Abril - 2001)",
+    answer: "Berlin",
+    position: 2,
+  },
+  {
+    id: 7,
+    question:
+      "¿Dónde fue el epicentro del terremoto que causó el tsunami del 2004?",
+    answer: "Rome",
+    position: 3,
+  },
+  {
+    id: 8,
+    question: "¿Con quien se casó el rey Charles en 2005?",
+    answer: "Madrid",
+    position: 4,
+  },
+];
+
+const MUSIC = [
+  {
+    id: 9,
+    question:
+      "¿En qué año Janet Jackson tuvo su 'accidente' de vestuario durante el Super Bowl?",
+    answer: "Paris",
+    position: 1,
+  },
+  {
+    id: 10,
+    question:
+      "¿Cuál Destiny's Child fue la primera en llegar al número uno en los Billboard como solista?",
+    answer: "Berlin",
+    position: 2,
+  },
+  {
+    id: 11,
+    question:
+      "¿En qué premios y qué fue la pelea entre Taylor Swift y Kanye West?",
+    answer: "Rome",
+    position: 3,
+  },
+  {
+    id: 12,
+    question: "¿Con quien se casó el rey Charles en 2005?",
+    answer: "Madrid",
+    position: 4,
+  },
+];
+
+const TECHNOLOGY = [
+  {
+    id: 13,
+    question: "¿Cuánto pagó Google por Youtube en 2006?",
+    answer: "Paris",
+    position: 1,
+  },
+  {
+    id: 14,
+    question: "¿En qué año se fundó MySpace?",
+    answer: "Berlin",
+    position: 2,
+  },
+  {
+    id: 15,
+    question: "¿En qué año se sube el primer video a Youtube?",
+    answer: "Rome",
+    position: 3,
+  },
+  {
+    id: 16,
+    question: "¿En qué año sale el primer iPhone?",
+    answer: "Madrid",
+    position: 4,
+  },
+];
+
+const SPORTS = [
+  {
+    id: 17,
+    question: "¿Qué país ganó la Eurocopa 2004?",
+    answer: "Paris",
+    position: 1,
+  },
+  {
+    id: 18,
+    question:
+      "¿Qué país ganó la medalla de oro en voleibol femenino en los Juegos Olímpicos de Sydney de 2000?",
+    answer: "Berlin",
+    position: 2,
+  },
+  {
+    id: 19,
+    question:
+      "¿Cuantas medallas ganó Usain Bolt en los Juegos Olímpicos de 2008",
+    answer: "Rome",
+    position: 3,
+  },
+  {
+    id: 20,
+    question:
+      "¿Contra quien jugó Chile la final de tennis en pareja en los Juegos Olímpicos de Atenas de 2004?",
+    answer: "Madrid",
+    position: 4,
+  },
+];
+
+export const QUESTIONS = {
+  CINEMA: CINEMA,
+  NEWS: NEWS,
+  MUSIC: MUSIC,
+  TECHNOLOGY: TECHNOLOGY,
+  SPORTS: SPORTS,
+};
