3fb223a8b1d82028f97ac09af3a71f3a4f234424
Victor Colina <victorcolina@G500606MAC272.local>
2024-09-04 00:17:19 -0400

    feat: vercel adapter agregado

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,8 +13,6 @@ export default defineConfig({
   },
 
   output: "server",
-
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
+
