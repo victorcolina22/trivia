6a9850d97aec304ec5bc9bb726332a1256c42caf
Victor Colina <victorcolina@G500606MAC272.local>
2024-09-04 00:07:50 -0400

    feat: node adapter agregado

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   devToolbar: {
     enabled: false,
   },
+
   output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
