cdd60f5d812e0f0b0c855732d6d7eeb2a979002e
Victor Colina <victorcolina@G500606MAC272.local>
2024-09-03 23:58:29 -0400

    feat: segundo juego agregado

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+import disneyAnswers from "./assets/disneyAnswers.json";
+import mostMedalsOlympicVolley from "./assets/mostMedalsOlympicVolley.json";
+import mostVisitedCountries from "./assets/mostVisitedCountries.json";
+import mostPopularBoybands from "./assets/mostPopularBoybands.json";
+
+export const DISNEY_ANSWERS = disneyAnswers;
+export const MOST_MEDALS_OLYMPIC_VOLLEY = mostMedalsOlympicVolley;
+export const MOST_VISITED_COUNTRIES = mostVisitedCountries;
+export const MOST_POPULAR_BOYBANDS = mostPopularBoybands;
+
+export const TEXT = {
+  1: "Películas animadas de Disney que más dinero generaron en el mundo.",
+  2: "Países con el medallero olímpico histórico de voleibol en los juegos olímpicos.",
+  3: "Países más visitados del mundo según la OMT.",
+  4: "Boybands más populares de todos los tiempos.",
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "custom-blue": "#3b55cd",
+        "custom-purple": "#3D2699",
+        "custom-purple-dark": "#23126A",
+        "purple-light": "#D7CDFF",
+      },
+      backgroundImage: {
+        "radial-gradient":
+          "radial-gradient(circle, rgba(156, 132, 248, 1) 0%, rgba(71, 48, 164, 1) 100%)",
+      },
+      borderColor: {
+        purple: "#3D2699",
+      },
+      colors: {
+        "purple-dark": "#23126A",
+      },
+    },
     screens: {
       "3xl": "1920px",
     },
